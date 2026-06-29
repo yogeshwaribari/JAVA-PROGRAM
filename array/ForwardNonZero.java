34e4b2b0dc07c6db13126164923ec16572742d64
Yogeshwari <yogeshwaribari@gmail.com>
2026-06-29 21:51:42 +0530

    update file

--- array/ForwardNonZero.java
+++ array/ForwardNonZero.java
@@ -12,4 +12,45 @@ Traverse the array and shift all non-zero elements forward while keeping their o
 then place all zeros at the remaining positions at the end.
 */
 import java.util.Scanner;
+
 class ForwardNonZero
+{
+    public static void main(String x[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements");
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int j = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] != 0)
+            {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+
+        while(j < n)
+        {
+            arr[j] = 0;
+            j++;
+        }
+
+        System.out.println("Array after moving zeros to end");
+        for(int i=0; i<n; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
