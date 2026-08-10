95b34db88cf576742152a3fab84559558fa971b1
Yogeshwari <yogeshwaribari@gmail.com>
2026-08-10 22:29:07 +0530

    Add file

---
+++ oops/abstract/Q61Delivery.java
@@ -0,0 +1,82 @@
+/*Question 61: Create abstract class Delivery.
+Asked In Practice Question
+Abstract method calculateCharges().
+Subclasses:
+
+LocalDelivery
+
+InternationalDelivery
+
+Input
+Weight: 2kg
+Rate: 50
+Output
+Charges: 100*/
+import java.util.*;
+abstract class Delivery
+{
+	abstract void calculateCharges();
+	int weight;
+	int rate;
+	Delivery(int weight,int rate)
+	{
+		this.weight=weight;
+		this.rate=rate;
+	}
+}
+class LocalDelivery extends Delivery
+{
+	LocalDelivery(int weight,int rate)
+	{
+		super(weight,rate);
+	}
+	void calculateCharges()
+	{
+		System.out.println("Charges :"+(weight*rate));
+	}
+}
+class InternationalDelivery extends Delivery
+{
+	InternationalDelivery(int weight,int rate)
+	{
+		super(weight,rate);
+	}
+	void calculateCharges()
+	{
+		System.out.println("Charges :"+(weight*rate*1.5));
+	}
+}
+class Q61Delivery
+{
+	public static void main(String x[])
+	{
+		Scanner xyz=new Scanner(System.in);
+		System.out.println("1.LocalDelivery\n2.InternationalDelivery");
+		System.out.println("Enter choice");
+		int choice=xyz.nextInt();
+		Delivery d;
+		switch(choice)
+		{
+			case 1:
+			System.out.println("Enter weight");
+			int weight=xyz.nextInt();
+			System.out.println("Enter rate");
+			int rate=xyz.nextInt();
+			d=new LocalDelivery(weight,rate);
+			d.calculateCharges();
+			break;
+			
+			case 2:
+			System.out.println("Enter weight");
+			 weight=xyz.nextInt();
+			System.out.println("Enter rate");
+			 rate=xyz.nextInt();
+			d=new InternationalDelivery(weight,rate);
+			d.calculateCharges();
+			break;
+			
+			default :
+			System.out.println("Wrong choice");
+		}
+	}
+}
